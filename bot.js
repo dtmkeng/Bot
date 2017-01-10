f3600bfe10380d24cb8bad4ce2979b6ca91d1667
Phumai Chanphunga <kang081@hotmail.com>
2017-01-10 23:16:33 +0700

    Update bot.js
    delete

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var login = require("facebook-chat-api");
 
 // Create simple echo bot
-login({email: "kang081@hotmail.com", password: "@root#keng"}, function callback (err, api) {
+login({email: "", password: ""}, function callback (err, api) {
     if(err) return console.error(err);
 
     api.listen(function callback(err, message) {
